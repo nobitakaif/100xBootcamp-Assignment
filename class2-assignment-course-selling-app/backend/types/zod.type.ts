668e4cd4b59b7fec120e6778a75162bbc422f97a
nobitakaif <mk2818356@gmail.com>
2026-01-23 17:35:08 +0000

    add types and schema. also fix the path

---
+++ class2-assignment-course-selling-app/backend/types/zod.type.ts
@@ -0,0 +1,29 @@
+
+import {  z } from "zod"
+
+export const SignupSchema = z.object({
+    email : z.email(),
+    password : z.string().min(6,{message : "too short"}).max(20,{message : "too large"}),
+    name : z.string().min(3,{message : "too short"}).max(18, {message : "too large"}),
+    role : z.enum(["Instructor", "Student"]),
+})
+
+export const LoginSchema = z.object({
+    email : z.email(),
+    password : z.string().min(6,{message : "too short"}).max(20,{message : "too large"})
+})
+
+export const CreateCourseSchema = z.object({
+    title : z.string().min(3,{message : "too short"}).max(18, {message : "too large"}),
+    description : z.string().min(3,{message : "too short"}).max(120, {message : "too large"}),
+    price : z.float32().min(1,{message : "price should be atleast 1.00 rs"}).max(1000000, {message : "price should be under 1000000"})
+})
+
+export const CreateLessonSchema = z.object({
+    title :  z.string().min(3,{message : "too short"}).max(18, {message : "too large"}),
+    content : z.string().min(30, {message : "atleast lesson should be 30 character"}).max(5000000, {message : "Lesson is too big this should be under 5000000 letter"})
+})
+
+export const PurchaseCourseSchema = z.object({
+    courseId : z.string().min(3, {message : "course-id is too short"}).max(40,{message : "course-id is too large "})
+})
